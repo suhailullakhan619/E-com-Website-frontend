7d43857086e56d52ee4834a73e62eae5f56852cd
myfirst-demo <suhailullkhan619@gmail.com>
2025-09-01 17:00:00 +0530

    frontend

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,51 +1,61 @@
 import React, { useEffect, useState } from 'react'
 'use client'
-import {  useSearchParams,useNavigate } from 'react-router-dom'
-import {  NavLink } from 'react-router-dom'
+import { useSearchParams, useNavigate } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './Header.css'
 
-const Header = ({cart=[]}) => {
-  const [search,setSearch]=useState('')
-let totalquantity =0;
-cart.forEach((cartitem)=>{
-  totalquantity+=cartitem.quantity;
-  
-})
-function navbartext(e){
-setSearch(e.target.value)
-}
-const [searchParams]=useSearchParams()
-const navigate=useNavigate()
+const Header = ({ cart = [] }) => {
+  const [search, setSearch] = useState('')
+  let totalquantity = 0;
+  cart.forEach((cartitem) => {
+    totalquantity += cartitem.quantity;
 
-useEffect(()=>{
-const searchparam=searchParams.get('search')||'';
-setSearch(searchparam)
-},[searchParams])
+  })
+  function navbartext(e) {
+    setSearch(e.target.value)
+  }
+  const [searchParams] = useSearchParams()
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    const searchparam = searchParams.get('search') || ''
+    setSearch(searchparam)
+  },[])
+
+function handleSearch(e){
+            e.preventDefault();
+          navigate(`/?search=${search}`); 
+          setSearch('');
+           }
 
 
   return (
     <>
-     <div className="header">
-      <div className="left-section">
-        <NavLink to="/" className="header-link">
-          <p className='logowithname'><img className="logo"
-            src= {'/images/mobile-logo-white.png'}/>SuhailUllaKhan</p>
-          
-          <img className="mobile-logo"
-            src={'/images/mobile-logo-white.png'} />
-        </NavLink>
-      </div>
+      <div className="header">
+        <div className="left-section">
+          <NavLink to="/" className="header-link">
+            <p className='logowithname'><img className="logo"
+              src={'/images/mobile-logo-white.png'} />SuhailUllaKhan</p>
 
-      <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" value={search} onChange={navbartext} />
+            <img className="mobile-logo"
+              src={'/images/mobile-logo-white.png'} />
+          </NavLink>
+        </div>
 
-         
-        <button className="search-button " onClick={()=>{navigate(`/?search=${search}`),setSearch('')}}>
+
+        <div className="middle-section">
+
+          <input className="search-bar" type="text" placeholder="Search" value={search} onChange={navbartext} />
+
+
+          <button className="search-button " onClick={handleSearch}>
           <img className="search-icon" src={"/images/icons/search-icon.png"} />
         </button>
-        
+
+
+
       </div>
+
 
       <div className="right-section">
         <NavLink className="orders-link header-link" to="/orders">
@@ -54,12 +64,12 @@ setSearch(searchparam)
         </NavLink>
 
         <NavLink className="cart-link header-link" to="/checkout">
-          <img className="cart-icon" src={ "/images/icons/cart-icon.png"} />
+          <img className="cart-icon" src={"/images/icons/cart-icon.png"} />
           <div className="cart-quantity">{totalquantity}</div>
           <div className="cart-text">Cart</div>
         </NavLink>
       </div>
-    </div>
+    </div >
     </>
   )
 }
